e1456978ab18374d2bfbb1fb38294046f5f45c62
Paul Jung <pbnjcub@gmail.com>
2023-02-09 19:04:25 -0500

    commit tree 0d41bd966f580fb2fc2b0a4baa6328e1e3bf1f31

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,23 @@
-#write your code here
+# #write your code here
+
+# def coundown(int)
+#     while int > 0
+#         puts "#{int} SECOND(S)!"
+#         int -= 1
+#     end
+#     "HAPPY NEW YEAR!"
+# end
+
+# def countdown_with_sleep(int)
+#     while int > 0
+#         countdown(int)
+#         sleep(1)
+#     end
+#     "HAPPY NEW YEAR!"
+# end
+
+
+
 
 def countdown(int)
     x = int
